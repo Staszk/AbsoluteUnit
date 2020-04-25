6885c3e231b1733a11d8fd3199b41605d21e9410
Parker <parkerstaszk@gmail.com>
2020-04-25 14:46:33 -0400

    UtilityAI Example

---
+++ Absolute Unit Testing/Assets/Scripts/Editor/RespawnSystemTestDriver.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AbsoluteUnit;
+
+public class RespawnSystemTestDriver : TestObject
+{
+    [Test]
+    static public Result RespawnSystemTestOne()
+    {
+        Vector3[] spawnPoints = new Vector3[]
+        {
+            new Vector3(564.992f, 163.1895f),
+            new Vector3(153.1190f, 892.6244f),
+        };
+
+        Vector3[] enemies = new Vector3[]
+        {
+            new Vector3(893.853f, 224.94729f),
+            new Vector3(153.1190f, 892.6244f),
+            new Vector3(38.644f, 63.95401f)
+        };
+
+        Vector3[] allies = new Vector3[]
+        {
+            new Vector3(153.1190f, 892.6244f),
+            new Vector3(468.135f, 295.114f)
+        };
+
+        int spawnIndex = RespawnSystem.GetBestRespawnPoint(spawnPoints, enemies, allies);
+        int expected = 1;
+
+        return Verify.AreEqual(expected, spawnIndex, "Correctly spawn away from enemies.");
+    }
+
+    [Test]
+    static public Result RespawnSystemTestTwo()
+    {
+        Vector3[] spawnPoints = new Vector3[]
+        {
+            new Vector3(564.992f, 163.1895f),
+            new Vector3(153.1190f, 892.6244f),
+        };
+
+        Vector3[] allies = new Vector3[]
+        {
+            new Vector3(893.853f, 224.94729f),
+            new Vector3(153.1190f, 892.6244f),
+
+        };
+
+        Vector3[] enemies = new Vector3[]
+        {
+            new Vector3(153.1190f, 892.6244f),
+            new Vector3(468.135f, 295.114f),
+            new Vector3(38.644f, 63.95401f)
+        };
+
+        int spawnIndex = RespawnSystem.GetBestRespawnPoint(spawnPoints, enemies, allies);
+        int expected = 1;
+
+        return Verify.AreEqual(expected, spawnIndex, "Correctly spawn near allies.");
+    }
+}
---
+++ Absolute Unit Testing/Assets/Scripts/RespawnSystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public static class RespawnSystem
+{
+    private struct RespawnPoint
+    {
+        public Vector3 pos;
+        public int idFromArray;
+        public int value;
+    };
+
+    public static int GetBestRespawnPoint(Vector3[] respawnPoints, Vector3[] enemies, Vector3[] allies)
+    {
+        RespawnPoint[] points = new RespawnPoint[respawnPoints.Length];
+
+        for (int i = 0; i < respawnPoints.Length; i++)
+        {
+            points[i] = new RespawnPoint()
+            {
+                pos = respawnPoints[i],
+                idFromArray = i,
+                value = 0
+            };
+        }
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            EvaluateRespawnPoint(ref points[i], enemies, allies);
+        }
+
+        points = points.OrderBy(p => p.value).ToArray();
+
+        return points[0].idFromArray;
+    }
+
+    private static void EvaluateRespawnPoint(ref RespawnPoint point, Vector3[] enemies, Vector3[] allies)
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if ((enemies[i] - point.pos).sqrMagnitude < 10)
+            {
+                point.value += 100;
+            }
+        }
+
+        for (int i = 0; i < allies.Length; i++)
+        {
+            if ((allies[i] - point.pos).sqrMagnitude < 6)
+            {
+                point.value += 49;
+            }
+        }
+    }
+}
